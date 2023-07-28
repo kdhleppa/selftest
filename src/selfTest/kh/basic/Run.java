7f7708e988cc8aa4920a6fc3bcd5dccd2aa04b58
kdhleppa <newkdh8624@gmail.com>
2023-07-28 17:44:23 +0900

    m

--- src/selfTest/kh/basic/Run.java
+++ src/selfTest/kh/basic/Run.java
@@ -8,7 +8,7 @@ public class Run {
 		YearningScore ys = new YearningScore();
 		String[] name = {"mike" , "tom", "ace"};
 		int[] yearning = {5,10,15};
-		String[][] photo = {{"mike","alice", "tom", "kim"} , {null,null,null,null}};
+		String[][] photo = {{"mike","alice", "tom", "kim"} , {"tom","ace","mike","alice"}};
 		System.out.println(ys.solution(name, yearning, photo));
 		
 		
--- src/selfTest/kh/basic/YearningScore.java
+++ src/selfTest/kh/basic/YearningScore.java
@@ -3,33 +3,31 @@ package selfTest.kh.basic;
 public class YearningScore {
 	
 	public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = {};
+		int[] answer = {};
         int count = 0;
         
         
-        //1. 포토배열과 네임배열에서 겹치는 값이 있을때
-        //2. count++ 해주고
-        //3. 네임배열인덱스 순서에 맞는 이여닝 인덱스순서값을 answer에 순차적대입
         
-        for (int i = 0; i < photo[0].length; i++) { // i값이 0 , 1, 2, 3순차로 들어감
-        	for (int j = 0; j < name.length; j++) { // name 인덱스값 j를 순차적으로 돌림
-                if (photo[0][i].equals(name[j])) { //포토 0행 0 값 mike값이 name 0 mike 값과 같을때
-                	count++;
-                	
-                	}
+        for (int i = 0; i < photo.length; i++) { 
+        	for (int j = 0; j < photo[i].length; j++) {
+        		for (int k = 0; k < name.length; k++) {
+                if (photo[i][j].equals(name[k])) { 
+                	answer[i] += yearning[k];
                 }
+            }
+            }
         	}
-        
+        /*
         for (int i = 0; i < photo[0].length; i++) {
         	for (int j = 0; j < name.length; j++) {
         		if (photo[0][i].equals(name[j])) {
-        			for (int k = 0; k < count; k++ ) {
-        			answer[k] = yearning[j];
+        			answer[0] += yearning[j];
         			}
         			
-        		}
+        		
         	}
         }
+        */
         
         
           
