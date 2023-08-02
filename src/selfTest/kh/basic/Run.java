35270c2bf6427325a4926b5b73a3cc5ee204561f
kdhleppa <newkdh8624@gmail.com>
2023-08-02 17:42:46 +0900

    0802

--- src/selfTest/kh/basic/Run.java
+++ src/selfTest/kh/basic/Run.java
@@ -4,13 +4,15 @@ public class Run {
 
 	public static void main(String[] args) {
 
-		
+		/*
 		YearningScore ys = new YearningScore();
 		String[] name = {"mike" , "tom", "ace"};
 		int[] yearning = { 5, 10, 15};
 		String[][] photo = {{"mike","alice", "tom", "kim"} , {"tom","ace","mike","alice"}};
 		System.out.println(ys.solution(name, yearning, photo));
-		
+		*/
+		nimNumArray array = new nimNumArray();
+		System.out.println(array.solution("3141592", "271"));
 		
 	}
 
--- src/selfTest/kh/basic/nimNumArray.java
+++ src/selfTest/kh/basic/nimNumArray.java
@@ -1,31 +1,47 @@
 package selfTest.kh.basic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class nimNumArray {
 	
 	public int solution(String t, String p) {
         int answer = 0;
-        int conunt = 0;
-        int sum = 0;
-        int[] arr = new int[t.length()];
+        int count = 0;
+        int num = 0;
+        
+        char[] arr = new char[t.length()];
+        int[] arr3 = new int[t.length()];
+        // List<Integer> arr2 = new ArrayList<>();
+        int[] arr2 = new int[(arr3.length)-(p.length()) +1];
         // 1. p.length 만큼 t 0~2 1~3 2~4 씩 자르기 
         // 1-2 charAt 으로 쪼개서 배열에 입력 ex) 3141592 = {3,1,4,1,5,9,2}
         // 1-3 3*10^p.length-1 + 1*10^p.length-2 + 4*10^p.length-3 
         // 2. 크기 비교후 count++으로 result구하기
         for (int i = 0 ; i < t.length(); i++) {
-            arr[i] = p.charAt(i);
-            
+            arr[i] = t.charAt(i);
         }
-        for (int i = 0; i < arr.length-p.length(); i++){
-            for (int j = 0; j < p.length(); j++) {
-                sum = (arr[i]*(10^(p.length()-1-j)));
-                i++;
-                sum += sum;
-            }
-            
+        for (int i = 0; i < arr.length; i++) {
+        	arr3[i] = arr[i]-'0';
         }
         
-        
-        
+        for (int i = 0; i < (arr3.length)-(p.length()) +1 ; i++){ // 위 예시 7- 3 = 4까지감 0 1 2 3 인덱스
+        	int sum = 0;
+        	for (int j = 0; j < p.length(); j++) {
+            	double k = p.length()-1-j;
+                num = (int) (arr3[i+j] * ( Math.pow (10, k) ) );
+                sum += num;
+                
+            } arr2[i]=sum;
+            
+        }
+        for (int i = 0 ; i < arr2.length; i++) {
+        	if (arr2[i] <= Integer.parseInt(p)) {
+        		
+        		count++;
+        	}
+        }
+        answer = count;
         return answer;
     }
 
